bdd2ac3265e4eb038eb59e413ed6b3efe0cc93dc
ackemag <74627057+ackemag@users.noreply.github.com>
2025-09-09 11:31:51 +0200

    Update task1b.m
    updated for task2 use

--- Homework2/task1b.m
+++ Homework2/task1b.m
@@ -1,5 +1,5 @@
 % Task 1b: Rescue pinger range calculation
-
+% Switch DI from 0 to 13.8 for Task2 2
 SL = 160.5;      % Source Level [dB re 1 uPa @ 1m]
 NL = 41.5;       % Noise Level [dB]
 DI = 0;          % Directivity Index [dB]
@@ -16,3 +16,4 @@ range_guess = 3000;                         % initial guess [m]
 r_solution = fzero(@(r) TLfun(r) - TLmax, range_guess);
 
 fprintf('Maximum detection distance ≈ %.2f km\n', r_solution/1000);
+
